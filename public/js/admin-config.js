3aefc9c644fe9587011ee53e424f644ddfad59e5
Aaron Johnson <aarontropy@gmail.com>
2014-03-22 11:02:07 -0400

    updated layout using multiple extends and got basic ui.routing in admin app

--- public/js/admin-config.js
+++ public/js/admin-config.js
@@ -1,1 +1,11 @@
 'use strict';
+
+angular.module('classy.admin').config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    // for unmatched routes:
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider.state('dashboard', {
+        url: '/',
+        templateUrl: '/views/admin/dashboard.html'
+    });
+}]);
