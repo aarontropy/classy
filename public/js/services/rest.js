0986f4f534706aa8f8a0d00e0c1651f5f31aac8c
Aaron Johnson <aarontropy@gmail.com>
2014-03-24 15:48:17 -0400

    cleaning up deleted files

--- public/js/services/rest.js
+++
@@ -1,9 +0,0 @@
-angular.module('classy').factory('SimpleRest', ['$resource', function($resource) {
-    return function(resource) {
-        return $resource('/' + resource + '/:id', { id: '@_id'}, {
-            update: {
-                method: 'PUT'
-            }
-        });
-    }
-}]);
